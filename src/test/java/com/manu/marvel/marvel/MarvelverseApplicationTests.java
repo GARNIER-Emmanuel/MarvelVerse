5bfcf8a088786535c8c7ec5ed886abf6cb7d79d5
manub <manu.boidun@gmail.com>
2025-07-09 14:27:36 +0200

    push init

---
+++ src/test/java/com/manu/marvel/marvel/MarvelverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manu.marvel.marvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
